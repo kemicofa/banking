64b2a1726ebfdf2969e78e60c527cf18f17889bd
Kevin Faulhaber <kevin.faulhaber@gmail.com>
2022-09-17 16:27:49 +0200

    feat: account balance and vailable balance fields

--- src/account.rs
+++ src/account.rs
@@ -1,16 +1,26 @@
 pub struct Account {
-    balance: u64
+    /// The available balance is the amount one can use for purchases or withdrawals.
+    /// It's made up of the account balance minus pending debit card authorizations and holds on funds.
+    available_balance: i64,
+    /// The account balance is made up of all posted credit and debit transactions.
+    /// It’s the amount you have in the account before any pending charges are added.
+    account_balance: i64
 }
 
 impl Account {
-    pub fn new(default_balance: u64) -> Account {
+    pub fn new(default_account_balance: i64) -> Account {
         Account {
-            balance: default_balance
+            available_balance: default_account_balance,
+            account_balance: default_account_balance
         }
     }
 
-    pub fn get_balance(&self) -> u64 {
-        self.balance
+    pub fn get_available_balance(&self) -> i64 {
+        self.available_balance
+    }
+
+    pub fn get_account_balance(&self) -> i64 {
+        self.account_balance
     }
 }
 
@@ -19,8 +29,14 @@ mod tests {
     use super::Account;
 
     #[test]
-    fn it_should_create_an_account_wth_default_balance() {
-        let account = Account::new(0);
-        assert_eq!(account.balance, 0);
+    fn it_should_create_an_account_wth_default_account_balance() {
+        let account = Account::new(100);
+        assert_eq!(account.account_balance, 100);
+    }
+
+    #[test]
+    fn it_should_create_an_account_with_matching_available_balance() {
+        let account = Account::new(100);
+        assert_eq!(account.available_balance, 100);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use account::Account;
 
 fn main() {
     let account = Account::new(0);
-    println!("{}", account.get_balance());
+    println!("{}", account.get_account_balance());
+    println!("{}", account.get_available_balance());
 }
