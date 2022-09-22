de3333d0711e0f9a0e25fa9660e91cb869fb4d61
Kevin Faulhaber <kevin.faulhaber@gmail.com>
2022-09-22 23:38:14 +0200

    refactor: small improvements

--- src/application/features/open_bank_account.rs
+++ src/application/features/open_bank_account.rs
@@ -1,17 +1,19 @@
 use crate::application::ports::{
-    bank_account_dto::{self, BankAccountDTO},
+    bank_account_dto::BankAccountDTO,
     bank_account_repository::BankAccountRepository,
 };
 
 use super::super::domain::bank_account::BankAccount;
 
-pub fn open_bank_account(bankRepository: Box<dyn BankAccountRepository>) -> BankAccount {
+pub fn open_bank_account(bank_repository: Box<dyn BankAccountRepository>) -> BankAccount {
     let account = BankAccount::new(0);
     let bank_account_dto = BankAccountDTO::new(
         account.get_id(),
         account.get_account_balance(),
         account.get_overdraft_amount(),
     );
-    bankRepository.insert(bank_account_dto);
+    let _ = bank_repository.insert(bank_account_dto);
+    // TODO: error handling here
+    // before return result
     account
 }
